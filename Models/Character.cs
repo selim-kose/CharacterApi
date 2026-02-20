35f4f07b0439fd617658759d0d7932b211af0b98
selim <selim-kose@live.com>
2026-02-20 16:17:18 +0100

    add character model

--- Models/Character.cs
+++ Models/Character.cs
@@ -1,4 +1,8 @@
 ﻿namespace CharacterApi.Models {
     public class Character {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string game { get; set; }
+        public string Role { get; set; }
     }
 }
