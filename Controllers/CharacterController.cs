df034e329632230acb0422b4b56c7addd1299a05
Selim Köse <107941258+selim-kose@users.noreply.github.com>
2026-02-24 14:05:10 +0100

    Charactermodel (#2)
    * Add character model
    
    * add character model
    
    * Complete projet

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using CharacterApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using VideoGameCharacterApi.Dtos;
+
+namespace CharacterApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController(ICharacterService service) : ControllerBase {
+
+        [HttpGet]
+        public async Task<ActionResult<List<CharacterResponse>>> GetCharacters()
+        => Ok(await service.GetAllCharactersAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CharacterResponse>> GetCharacter(int id) {
+            var character = await service.GetCharacterByIdAsync(id);
+            return character is null ? NotFound("Character with the given Id was not found.") : Ok(character);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CharacterResponse>> AddCharacter(CreateCharacterRequest character) {
+            var createdCharacter = await service.AddCharacterAsync(character);
+            return CreatedAtAction(nameof(GetCharacter), new { id = createdCharacter.Id }, createdCharacter);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCharacter(int id, UpdateCharacterRequest character) {
+            var updated = await service.UpdateCharacterAsync(id, character);
+            return updated ? NoContent() : NotFound("Character with the given Id was not found.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCharacter(int id) {
+            var deleted = await service.DeleteCharacterAsync(id);
+            return deleted ? NoContent() : NotFound("Character with the given Id was not found.");
+        }
+    }
+}
---
+++ Services/ICharacterService.cs
@@ -0,0 +1,12 @@
+﻿using CharacterApi.Models;
+using VideoGameCharacterApi.Dtos;
+
+namespace CharacterApi.Services {
+    public interface ICharacterService {
+        Task<List<CharacterResponse>> GetAllCharactersAsync();
+        Task<CharacterResponse?> GetCharacterByIdAsync(int id);
+        Task<CharacterResponse> AddCharacterAsync(CreateCharacterRequest character);
+        Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequest character);
+        Task<bool> DeleteCharacterAsync(int id);
+    }
+}
